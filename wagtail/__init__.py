b70959a4fe1374c5c285aa33d115b2ede64dddad
Karl Hobley <karl@torchbox.com>
2021-10-13 19:20:46 +0100

    Merge wagtail.core.__init__ with wagtail.__init__

--- wagtail/__init__.py
+++ wagtail/__init__.py
@@ -13,3 +13,19 @@ __version__ = get_version(VERSION)
 
 # Required for npm package for frontend
 __semver__ = get_semver_version(VERSION)
+# Imported for historical reasons
+from wagtail import __semver__, __version__  # noqa
+
+
+default_app_config = 'wagtail.core.apps.WagtailCoreAppConfig'
+
+
+def setup():
+    import warnings
+
+    from wagtail.utils.deprecation import removed_in_next_version_warning
+
+    warnings.simplefilter("default", removed_in_next_version_warning)
+
+
+setup()
